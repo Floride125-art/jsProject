f2e2625208028481a665de9aa89126b0cc8cce9f
Floride125-art <FTuyisenge120@daviscollege.com>
2020-09-27 19:05:10 +0200

    just finished working on my code

--- Javascript/form.js
+++ Javascript/form.js
@@ -5,9 +5,10 @@ var myMonth= document.getElementById("month").value;
 var myDay = document.getElementById("day").value;
 var genderM = document.querySelector('input[name="gender"]:checked').value;
 var genderF = document.querySelector('input[name="gender"]:checked').value;
+var girlsNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var dayWeek=["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
 var boysNames=["Kwasi", "Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
-var girlsNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
 var umunsi = ( ( (myCentury/4) -2*myCentury-1) + ((5*mYear/4) ) + ((26*(myMonth+1)/10)) + myDay ) % 7;
 umunsi= umunsi.toFixed(0);
 if(myDay <= 0 || myMonth <= 0){
@@ -24,6 +25,17 @@ else if(umunsi == 1 || genderM == male){
   alert("you were born on" +dayWeek[0])
       document.getElementById("answer").innerHTML=boysNames[0];
 }
+else if(umunsi == 1 || genderM == female){
+  alert("you were born on" +dayWeek[0])
+      document.getElementById("answer").innerHTML=girlsNames[0];
+  
+}
+
+else if(umunsi == 2 || genderM == female){
+  alert("you were born on" +dayWeek[1])
+      document.getElementById("answer").innerHTML=girlsNames[1];
+ 
+}
 else if(umunsi == 2 || genderM == male){
   alert("you were born on" +dayWeek[1])
       document.getElementById("answer").innerHTML=boysNames[1];
@@ -33,14 +45,37 @@ else if(umunsi == 3 || genderM == male){
   alert("you were born on" +dayWeek[2])
       document.getElementById("answer").innerHTML=boysNames[2];
 }
+else if(umunsi == 3 || genderF == female){
+  alert("you were born on" +dayWeek[2])
+      document.getElementById("answer").innerHTML=girlsNames[2];
+ 
+}
+
+else if(umunsi == 4 || genderF == female){
+  alert("you were born on" +dayWeek[3])
+      document.getElementById("answer").innerHTML=girlsNames[3];
+ 
+}
 else if(umunsi == 4 || genderM == male){
   alert("you were born on" +dayWeek[3])
       document.getElementById("answer").innerHTML=boysNames[3];
 }
+
+
 else if(umunsi == 5 || genderM == male){
   alert("you were born on" +dayWeek[4])
       document.getElementById("answer").innerHTML=boysNames[3];
   
+}
+else if(umunsi == 5 || genderF == female){
+  alert("you were born on" +dayWeek[4])
+      document.getElementById("answer").innerHTML=girlsNames[4];
+  
+}
+
+else if(umunsi == 6 || genderF == female){
+  alert("you were born on" +dayWeek[5])
+      document.getElementById("answer").innerHTML=girlsNames[5];
 }
 else if(umunsi == 6 || genderM == male){
   alert("you were born on" +dayWeek[5])
@@ -51,41 +86,34 @@ else if(umunsi == 7 || genderM == male){
       document.getElementById("answer").innerHTML=boysNames[6];
 
 }
-else if(umunsi == 1 || genderM == female){
-  alert("you were born on" +dayWeek[0])
-      document.getElementById("answer").innerHTML=girlsNames[0];
-  
-}
-else if(umunsi == 2 || genderM == female){
-  alert("you were born on" +dayWeek[1])
-      document.getElementById("answer").innerHTML=girlsNames[1];
- 
-}
-else if(umunsi == 3 || genderF == female){
-  alert("you were born on" +dayWeek[2])
-      document.getElementById("answer").innerHTML=girlsNames[2];
- 
-}
-else if(umunsi == 4 || genderF == female){
-  alert("you were born on" +dayWeek[3])
-      document.getElementById("answer").innerHTML=girlsNames[3];
- 
-}
-else if(umunsi == 5 || genderF == female){
-  alert("you were born on" +dayWeek[4])
-      document.getElementById("answer").innerHTML=girlsNames[4];
-  
-}
-else if(umunsi == 6 || genderF == female){
-  alert("you were born on" +dayWeek[5])
-      document.getElementById("answer").innerHTML=girlsNames[5];
-}
-
 else if(umunsi == 7 || genderF == female){
   alert("you were born on" +dayWeek[6])
       document.getElementById("answer").innerHTML=girlsNames[6];
 }else{
-  alert("your input are invalid!");
+  alert("your input is invalid!");
 }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
